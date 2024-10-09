b7f8f1942fa10480bc27814f7133331f31e95101
roxana correa <roxanacorrea33@gmail.com>
2024-10-09 01:46:46 -0300

    final actualizado

--- src/data/models/product.js
+++ src/data/models/product.js
@@ -1,16 +1,46 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    code: String,
-    price: Number,
-    stock: Number,
-    category: String,
-    thumbnails: [String],
-    status: Boolean,
+    title: {
+        type: String,
+        required: true, 
+    },
+    description: {
+        type: String,
+        required: true, 
+    },
+    code: {
+        type: String,
+        required: true, 
+        unique: true,   
+    },
+    price: {
+        type: Number,
+        required: true, 
+        min: 0,         
+    },
+    stock: {
+        type: Number,
+        required: true, 
+        min: 0,         
+    },
+    category: {
+        type: String,
+        required: true, 
+    },
+    thumbnails: {
+        type: [String], 
+        default: [],    
+    },
+    status: {
+        type: Boolean,
+        default: true, 
+    },
 });
 
+
 const Product = mongoose.model('Product', productSchema);
+
 export default Product;
+
 
